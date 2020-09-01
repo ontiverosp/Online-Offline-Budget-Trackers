25ec8a033174ac2a32c732f08fc8308b2cf273f1
Pablo Ontiveros <pablacas@gmail.com>
2020-09-01 05:03:43 -0500

    added dependancies

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,7 @@ const FILES_TO_CACHE = [
   "/index.html",
   "/styles.css",
   "/index.js",
-  "/dist/index.bundle.js",
+  "/dist/bundle.js",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png"
 ];
